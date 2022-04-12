9059f5936f4d46ba20c7b272fbe3afaa7abe6c1a
vivek patel <55041874+viveks-codes@users.noreply.github.com>
2022-04-12 07:56:17 +0530

    Init

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, flash, redirect, render_template, \
+     request, url_for
+import pandas as pd 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+cv = CountVectorizer(max_features=8000,stop_words='english')
+
+df = pd.read_csv('finalPrep.csv')
+
+vector = cv.fit_transform(df['tags']).toarray()
+
+
+similarity = cosine_similarity(vector)
+
+def rec(movie,n):
+    r = []
+    index = df[df['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])),reverse=True,key = lambda x: x[1])
+    for i in distances[1:int(n)+1]:
+        r.append(df.iloc[i[0]].title)
+    return r
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/recommend',methods=['POST'])
+def recommend():
+    movie = request.form['movie']
+    n = request.form['n']
+    print(rec(movie,n))
+    suggestions = rec(movie,int(n))
+    return render_template('index.html',suggestions=suggestions)
+if __name__ == '__main__':
+    app.run(debug=True)
